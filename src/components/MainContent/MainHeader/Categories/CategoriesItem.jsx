f09ed7551deda8d4dedc110211459794ef86e55d
Noch4nce <m4rgera123@mail.ru>
2022-09-27 17:07:26 +0500

    categories active

--- src/components/MainContent/MainHeader/Categories/CategoriesItem.jsx
+++ src/components/MainContent/MainHeader/Categories/CategoriesItem.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const categoriesItems = [
+	'Все',
+	'Мясные',
+	'Вегетарианская',
+	'Гриль',
+	'Острые',
+	'Закрытые'
+]
 
 const CategoriesItem = () => {
+	const [active, setActive] = useState('Все')
+
+	const handleActive = (e) => {
+		setActive(e.target.textContent)
+	}
+
 	return (
 		<>
-			<li className="active">Все</li>
-			<li>Мясные</li>
-			<li>Вегетарианская</li>
-			<li>Гриль</li>
-			<li>Острые</li>
-			<li>Закрытые</li>
+			{/*<li className="active">Все</li>*/}
+			{/*<li>Мясные</li>*/}
+			{/*<li>Вегетарианская</li>*/}
+			{/*<li>Гриль</li>*/}
+			{/*<li>Острые</li>*/}
+			{/*<li>Закрытые</li>*/}
+
+			{categoriesItems.map((item) => (
+				<li
+					onClick={(e) => handleActive(e)}
+					className={item === active ? 'active' : ''}
+				>
+					{item}
+				</li>
+			))}
 		</>
 	)
 }
