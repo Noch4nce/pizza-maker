be297d6d24b122be09d0087d2b09ed84a018fb94
Noch4nce <m4rgera123@mail.ru>
2022-09-29 20:15:25 +0500

    Cart comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './scss/app.scss'
 import Header from './components/Header/Header'
 import MainContent from './components/MainContent/MainContent'
+import Cart from './components/Cart/Cart'
 
 const App = () => (
-	<div className="wrapper">
-		<Header />
-		<MainContent />
-	</div>
+	// <div className="wrapper">
+	// 	<Header />
+	// 	<MainContent />
+	// </div>
+
+	<Cart />
 )
 
 export default App
