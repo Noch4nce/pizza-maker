63f76cc2f7eae54a4d326bdb196238fce60c3564
Noch4nce <m4rgera123@mail.ru>
2022-09-30 20:30:15 +0500

    fix routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './scss/app.scss'
 import MainContent from './components/MainContent/MainContent'
 import Cart from './components/Cart/Cart'
 import { Routes, Route } from 'react-router-dom'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+import Header from './components/Header/Header'
+import React from 'react'
 
 const App = () => (
-	<Routes>
-		<Route path="/" element={<MainContent />} />
-		<Route path="cart" element={<Cart />} />
-	</Routes>
+	<div className="wrapper">
+		<Header />
+		<Routes>
+			<Route path="*" element={<NotFoundPage />} />
+
+			<Route path="/" element={<MainContent />} />
+			<Route path="cart" element={<Cart />} />
+		</Routes>
+	</div>
 )
 
 export default App
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-import Header from '../Header/Header'
 import MainPage from '../../pages/MainPage/MainPage'
-import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage'
 
 const MainContent = () => {
 	return (
-		<div className="wrapper">
-			<Header />
-			<div className="content">
-				<div className="container">
-					{/*<MainPage />*/}
-					<NotFoundPage />
-				</div>
+		<div className="content">
+			<div className="container">
+				<MainPage />
 			</div>
 		</div>
 	)
