9751d86a759b3268c1ff4191e846c3fafe11d26b
Noch4nce <m4rgera123@mail.ru>
2022-10-04 15:33:24 +0500

    clear

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import clearSvg from '../../../assets/images/clear.svg'
 import styles from './styles.module.scss'
 
 const Search = ({ searchValue, onChangeSearchValue }) => {
 	return (
-		<input
-			className={styles.searchInput}
-			onChange={(event) => onChangeSearchValue(event.target.value)}
-			value={searchValue}
-			type="text"
-			placeholder="Поиск пиццы..."
-		/>
+		<div className={styles.searchWrapper}>
+			<input
+				className={styles.searchInput}
+				onChange={(event) => onChangeSearchValue(event.target.value)}
+				value={searchValue}
+				type="text"
+				placeholder="Поиск пиццы..."
+			/>
+			{searchValue && (
+				<img
+					onClick={() => onChangeSearchValue('')}
+					className={styles.searchClear}
+					src={clearSvg}
+					alt="clear"
+				/>
+			)}
+		</div>
 	)
 }
 
