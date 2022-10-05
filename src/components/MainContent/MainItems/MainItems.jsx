d539d4c462707f688c7e983a075a24458cfdaf94
Noch4nce <m4rgera123@mail.ru>
2022-10-05 17:35:04 +0500

    paginate fetch

--- src/components/MainContent/MainItems/MainItems.jsx
+++ src/components/MainContent/MainItems/MainItems.jsx
@@ -6,26 +6,34 @@ import Pagination from '../../Pagination/Pagination'
 const MainItems = ({ categoryId, sortSelectedTab, searchValue }) => {
 	const [pizzasData, setPizzasData] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
+	const [pageNumber, setPageNumber] = useState(0)
+	const [pageCount, setPageCount] = useState(0);
 	const fakeArray = [...Array(10).keys()]
 
 	useEffect(() => {
 		setIsLoading(true)
 		const sortByAll = `sortBy=${sortSelectedTab.type}`
 		const order = `&order=${sortSelectedTab.order}`
-		const searchByTitle = `&title=${searchValue}`
-		const sortByCategories = `category=${categoryId}&${sortByAll}${order}${searchByTitle}`
-
+		const searchByTitle = searchValue ? `&title=${searchValue}` : ''
+		const page = `&page=${pageNumber + 1}&limit=8`
+		const sortByCategories = `category=${categoryId}&${sortByAll}${order}${page}${searchByTitle}`
+		console.log(page, 'page')
 		fetch(
 			'https://63356b088aa85b7c5d1ad1db.mockapi.io/items?' +
 				(categoryId
 					? sortByCategories
-					: sortByAll + order + searchByTitle)
+					: sortByAll + order + page + searchByTitle)
 		)
 			.then((response) => response.json())
 			.then((data) => setPizzasData(data))
 			.finally(() => setIsLoading(false))
 		window.scrollTo(0, 0)
-	}, [categoryId, sortSelectedTab, searchValue])
+	}, [categoryId, sortSelectedTab, searchValue, pageNumber])
+
+	useEffect(() => {
+		setPageCount(Math.ceil(pizzasData.length / 8));
+	}, [pizzasData])
+	console.log(pageCount, "pageCountpageCount")
 
 	console.log(pizzasData, 'pizzasData')
 
@@ -64,7 +72,7 @@ const MainItems = ({ categoryId, sortSelectedTab, searchValue }) => {
 							})}
 			</div>
 
-			<Pagination />
+			<Pagination pageCount={pageCount} setPageNumber={setPageNumber} />
 		</>
 	)
 }
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactPaginate from 'react-paginate'
 import styles from './styles.module.scss'
 
-const Pagination = () => {
+const Pagination = ({ pageCount, setPageNumber }) => {
 	return (
 		<ReactPaginate
 			className={styles.paginateRoot}
 			breakLabel="..."
 			nextLabel=">"
-			onPageChange={(e) => console.log(e)}
-			pageRangeDisplayed={5}
-			pageCount={3}
+			onPageChange={(event) => setPageNumber(event.selected)}
+			pageRangeDisplayed={8}
+			pageCount={pageCount}
 			previousLabel="<"
 			renderOnZeroPageCount={null}
 		/>
