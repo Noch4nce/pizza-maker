88692b4c02284da7146444dd41969e4f20ce271e
Noch4nce <m4rgera123@mail.ru>
2022-10-05 15:42:04 +0500

    paginate styles

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactPaginate from 'react-paginate'
+import styles from './styles.module.scss'
 
 const Pagination = () => {
 	return (
 		<ReactPaginate
+			className={styles.paginateRoot}
 			breakLabel="..."
 			nextLabel=">"
 			onPageChange={(e) => console.log(e)}
