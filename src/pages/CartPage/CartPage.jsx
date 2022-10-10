916434fb5b1d226118705827ac463443efb21504
Noch4nce <m4rgera123@mail.ru>
2022-10-10 15:52:34 +0500

    show cart empty

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 import CartEmpty from '../../components/Cart/CartEmpty'
 import Cart from '../../components/Cart/Cart'
 
 const CartPage = () => {
+	const totalCartCount = useSelector(
+		(state) => state.cartReducer.totalCartCount
+	)
+
+	if (totalCartCount <= 0) {
+		return <CartEmpty />
+	}
+
 	return (
 		<div className="container--cart">
-			{/*<CartEmpty />*/}
 			<Cart />
 		</div>
 	)
