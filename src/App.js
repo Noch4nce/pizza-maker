c30e816ec6ed9876248554d4ffb6013eb8dab2fa
Noch4nce <m4rgera123@mail.ru>
2022-10-12 15:30:17 +0500

    main layout Outlet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import Header from './components/Header/Header'
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 import MainPage from './pages/MainPage/MainPage'
 import CartPage from './pages/CartPage/CartPage'
 import DetailInfoPage from './pages/DetailInfoPage/DetailInfoPage'
+import MainLayout from './layouts/MainLayout'
 import './scss/app.scss'
 
 const App = () => {
 	return (
-		<div className="wrapper">
-			<Header />
-			<div className="content">
-				<div className="container">
-					<Routes>
-						<Route path="*" element={<NotFoundPage />} />
+		<Routes>
+			<Route path="/" element={<MainLayout />}>
+				<Route path="*" element={<NotFoundPage />} />
 
-						<Route path="/" element={<MainPage />} />
-						<Route path="cart" element={<CartPage />} />
-						<Route path="/pizza/:id" element={<DetailInfoPage />} />
-					</Routes>
-				</div>
-			</div>
-		</div>
+				<Route path="" element={<MainPage />} />
+				<Route path="cart" element={<CartPage />} />
+				<Route path="pizza/:id" element={<DetailInfoPage />} />
+			</Route>
+		</Routes>
 	)
 }
 
