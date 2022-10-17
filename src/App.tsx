4e0e13010607e09677d97730bd10beeb72ee06e4
Noch4nce <m4rgera123@mail.ru>
2022-10-17 14:21:47 +0500

    lazy render

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 import MainPage from './pages/MainPage/MainPage'
-import CartPage from './pages/CartPage/CartPage'
-import DetailInfoPage from './pages/DetailInfoPage/DetailInfoPage'
 import MainLayout from './layouts/MainLayout'
 import './scss/app.scss'
+
+const CartPage = React.lazy(() => import('./pages/CartPage/CartPage'))
+const DetailInfoPage = React.lazy(
+	() => import('./pages/DetailInfoPage/DetailInfoPage')
+)
+const NotFoundPage = React.lazy(
+	() => import('./pages/NotFoundPage/NotFoundPage')
+)
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<MainLayout />}>
-				<Route path="*" element={<NotFoundPage />} />
+				<Route
+					path="*"
+					element={
+						<Suspense fallback={<div>Loading...</div>}>
+							<NotFoundPage />
+						</Suspense>
+					}
+				/>
 
 				<Route path="" element={<MainPage />} />
-				<Route path="cart" element={<CartPage />} />
-				<Route path="pizza/:id" element={<DetailInfoPage />} />
+
+				<Route
+					path="cart"
+					element={
+						<Suspense fallback={<div>Loading...</div>}>
+							<CartPage />
+						</Suspense>
+					}
+				/>
+
+				<Route
+					path="pizza/:id"
+					element={
+						<Suspense fallback={<div>Loading...</div>}>
+							<DetailInfoPage />
+						</Suspense>
+					}
+				/>
 			</Route>
 		</Routes>
 	)
