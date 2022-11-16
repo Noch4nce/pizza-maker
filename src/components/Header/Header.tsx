cc6227fa162491a22c2c128630651253cd0063e5
Noch4nce <m4rgera123@mail.ru>
2022-11-16 15:05:28 +0500

    hide search in cart

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
 import React, { FC } from 'react'
+import { useLocation } from 'react-router-dom'
+
 import HeaderLogo from './HeaderLogo/HeaderLogo'
 import HeaderCart from './HeaderCart/HeaderCart'
 import Search from './Search/Search'
 
 const Header: FC = () => {
+	const location = useLocation()
+
 	return (
 		<div className="header">
 			<div className="container">
 				<HeaderLogo />
-				<Search />
+				{location.pathname !== '/cart' && <Search />}
 				<HeaderCart />
 			</div>
 		</div>
